dd8a1ab327ef5619d08f412aa3411f1ad7b9f54b
AlexChihaia <alexchihaia84@gmail.com>
2023-11-16 09:56:45 +0100

    feat : correzione e uso di più funzioni

--- js/main.js
+++ js/main.js
@@ -4,17 +4,31 @@
 const startButton = document.getElementById('start-button');
 
 startButton.addEventListener('click', function () {
+    startGame();
+});
+
+
+function startGame() {
     const board = document.getElementById('board');
     board.innerHTML = '';
-
     for (let i = 1; i <= 100; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.textContent = i;
-        cell.addEventListener('click', function () {
-            cell.classList.add('clicked');
-            console.log('Cella cliccata:', i);
-        })
-        board.append(cell);
+        createCell(i);
     }
-});
+};
+
+
+function createCell(i) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.innerHTML = i;
+    cell.addEventListener('click', function () {
+        selectCell(cell);
+    });
+    document.getElementById('board').append(cell);
+};
+
+
+function selectCell(cell) {
+    cell.classList.add('clicked');
+    console.log('Hai selezionato la cella numero:', cell.innerHTML);
+}
